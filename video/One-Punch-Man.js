5162247b9ee8c57bfa3085daef1c85a6e74e125d
droidxrx <61324913+droidxrx@users.noreply.github.com>
2020-03-10 01:41:08 +0700

    Create One-Punch-Man.js

---
+++ video/One-Punch-Man.js
@@ -0,0 +1,6 @@
+function eps11080P() {
+    $("#nonton-bro").empty();var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/w8k296h");ifrm.setAttribute("allow","autoplay");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps1720P() {
+    $("#nonton-bro").empty();var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/v6yorra");ifrm.setAttribute("allow","autoplay");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
