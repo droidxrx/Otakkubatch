607e907b0ce52dbdc181d503e9c470c88be61813
droidxrx <61324913+droidxrx@users.noreply.github.com>
2020-03-10 10:40:28 +0700

    Update and rename One-Punch-Man.js to One Punch Man.js

---
+++ video/One Punch Man.js
@@ -0,0 +1,32 @@
+$('.dropbtn').attr('onClick', 'MyRemove()');
+function MyRemove() {
+    $("#nonton-bro").empty();var image=document.createElement("img");image.setAttribute("src","bg.jpg");document.getElementById("nonton-bro").appendChild(image);
+};
+//1080p
+function eps11080P() {
+    $("#nonton-bro").empty();var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/w8k296h");ifrm.setAttribute("allow","autoplay");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps21080P() {
+    $("#nonton-bro").empty();var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/t7escw4");ifrm.setAttribute("allow","autoplay");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps31080P() {
+    $("#nonton-bro").empty();var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/ujcdth6");ifrm.setAttribute("allow","autoplay");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps41080P() {
+    $("#nonton-bro").empty();var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/rsxv35x");ifrm.setAttribute("allow","autoplay");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+
+//720p
+
+function eps1720P() {
+    $("#nonton-bro").empty();var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/v6yorra");ifrm.setAttribute("allow","autoplay");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps2720P() {
+    $("#nonton-bro").empty();var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/sycgal6");ifrm.setAttribute("allow","autoplay");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps3720P() {
+    $("#nonton-bro").empty();var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/ts2f9kn");ifrm.setAttribute("allow","autoplay");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps4720P() {
+    $("#nonton-bro").empty();var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/rdah96c");ifrm.setAttribute("allow","autoplay");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
