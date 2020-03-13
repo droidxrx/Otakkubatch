be393a3ea4ffdad950a783b752cc3c05d47c250b
droidxrx <61324913+droidxrx@users.noreply.github.com>
2020-03-14 00:45:46 +0700

    Create Koe-no-Katachi.js

---
+++ video/Koe-no-Katachi.js
@@ -0,0 +1,12 @@
+$('.dropbtn').attr('onClick', 'MyRemove()');
+function MyRemove() {
+    $("#nonton-bro").empty();var image=document.createElement("img");image.setAttribute("src","https://cdn.statically.io/img/3.bp.blogspot.com/-Te2qDKpTrBo/XmcMr_B9UpI/AAAAAAAAALQ/dXJtN0yDoew0tdAck1hWSxg24JJSG9AggCLcBGAsYHQ/s1600/bg.jpg");document.getElementById("nonton-bro").appendChild(image);
+};
+//1080p
+function eps11080P() {
+    $("#nonton-bro").empty();var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://muvix.us/video/OabofqgrL4CEtmY/");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+//720p
+function eps1720P() {
+    $("#nonton-bro").empty();var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://muvix.us/video/gDfRT2d7SmF1ljJ/");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
