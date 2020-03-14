ad54963815ca735225eada7218b2758d45df2420
droidxrx <61324913+droidxrx@users.noreply.github.com>
2020-03-14 15:03:34 +0700

    Update opm.js

--- video/One Punch Man/opm.js
+++ video/One Punch Man/opm.js
@@ -1,8 +1,5 @@
 $('.dropbtn').attr('onClick', 'MyRemove()');
-function MyRemove() {
-    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
-    var image=document.createElement("img");image.setAttribute("src","https://cdn.statically.io/img/3.bp.blogspot.com/-Te2qDKpTrBo/XmcMr_B9UpI/AAAAAAAAALQ/dXJtN0yDoew0tdAck1hWSxg24JJSG9AggCLcBGAsYHQ/s1600/bg.jpg");document.getElementById("nonton-bro").appendChild(image);
-};
+function MyRemove() {$("#nonton-bro").empty()}
 //1080p
 function eps11080P() {
     $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
