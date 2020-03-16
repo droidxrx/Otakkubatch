5e47ee6abc924543da3b62b23b237ad437d0f117
droidxrx <61324913+droidxrx@users.noreply.github.com>
2020-03-16 22:47:51 +0700

    Create opms2.js

---
+++ video/One Punch Man/opms2.js
@@ -0,0 +1,109 @@
+$('.dropbtn').attr('onClick', 'MyRemove()');
+function MyRemove() {$("#nonton-bro").empty()}
+//1080p
+function eps01080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/uk42c4m");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps11080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/sowbplh");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps21080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/u6hqy4a");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps31080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/rpl4nrv");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps41080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/wsj7grt");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps51080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/wqvo7kz");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps61080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/sejo7p9");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps71080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/uxh27u8");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps81080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/seyrceu");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps91080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/qnqy729");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps101080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/tqmjqjh");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps111080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/vymddht");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps121080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/wzo27rg");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+
+//720p
+function eps0720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/wt5da9s");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps1720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/uxpdk33");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps2720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/rb69zpu");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps3720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/tcr4on7");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps4720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/wrt47k7");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps5720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/tnox8oz");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps6720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/s9gko8l");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps7720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/vvc6fkb");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps8720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/r5kp926");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps9720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/yx77vlsv");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps10720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/r3767tf");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps11720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/stke86l");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps12720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";
+    var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");ifrm.setAttribute("src","https://tinyurl.com/wroyfyq");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
