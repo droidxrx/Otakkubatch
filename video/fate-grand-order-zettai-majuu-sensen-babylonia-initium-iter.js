e6e4e76a280f230a62fe989f95809e1967b4eeb6
droidxrx <61324913+droidxrx@users.noreply.github.com>
2020-03-23 20:59:02 +0700

    Create fate-grand-order-zettai-majuu-sensen-babylonia-initium-iter.js

---
+++ video/fate-grand-order-zettai-majuu-sensen-babylonia-initium-iter.js
@@ -0,0 +1,21 @@
+$('.dropbtn').attr('onClick', 'MyRemove()');
+function MyRemove() {$("#nonton-bro").empty()}
+//1080p
+function eps11080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
+    ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=6gRprDNGQxtyNI9SS0T4Zwug5ikf9J%252BcvIQzw%252Bs%252F0v51rvpVyplVN2%252FM0vAkvnceTI9r2dpUUnEg%252Fy2eulp57MHnAh4Y0gwEQO%252FWrKmElBoX8%252Fcv%252BaFCR2q5ZyYQHeOtWHZ%252ByGNcPNbt15lrk%252BYvxxUbHy3Lb72P5G7g7vQ0dL1456UwqJf1i%252BI7NxQPxsOYp%252BU162nJBm9KqGO9LWRNNrVev7%252FYeEF3%252Bt%252BdYJnQIbyc1m1CGeWAGYpffvup%252B1jXSnMUIfwFExE9YUDqAsza0%252BLwZXETxS0vCzUO5TlwuuVvL9rg16W1%252B4i97DP7tRdwrNIxmPB2ja6jdFVP8zUmkq");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps21080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
+    ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=jrxL3%252FbQF62GjF1RJnUJdwB58ThwA%252FRWUrnH43XO86%252FQUowQzPaVdghTQ%252FMibOm9Ev58%252FtE4znbuVUWpWMG6hY7XKefbizR3ReB8smSzuMVFPfDMIAtcGbgbwpp0JsR4mbeSA%252FZzcc163%252FRFZKMXNEymS3B%252F6Xc4SoKoMAxdTOsBVzkRWDc04JkvMl%252B8pLNT7yIAPY%252FpjnALBoEDFnDJfeBOXXVvljvZ8cjYmVpfItWJiSVZUscTG0pqeWwTcL4OZmmk0P%252FOv16TVPUg4MjYtCMR6RH3bhwyhIZN7%252FS1GGMJ2eYQ3EsOQpbipP%252F9jcCvcaACl2w8daYdFC%252B54EsQoj");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+
+//720p
+function eps1720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
+    ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=7NiGubzIfwscJOk0WRUKPw1vG6CkDO848HoLHvDDH2ASd3xVB3aYx%252FZNava1R24jtKwGkpAswmPegmRsuWAjEVg%252FPE5OFQUwFULSnxDtXWlLN6rV2E9e2gzP%252FbY7jR1RkDDyDgU6rKFG4Bu3wWmvBvHf5%252FhjDX%252FUIrzC7keCuNXoYnhndL9gDMudPsAjyYoHM%252Fvyu5xuNo%252BNBL4IEl0vZD4d88gl2Ssn5RrfUPRPyAG3jK7xB0RYBiy%252FPJUhOOwQq17ydeFNd5ADhgzjRkI1rXWoQ5ZgQF5c%252FY%252BWsyiyItTG8a5lNFdsfqYrgTzhrMVugDMd9kcSsy8FIcJbF%252FrClv");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps2720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
+    ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=6WDBcCjcNmUQpoNW0OCw5AI%252BuszP9KF9WjqTuwnvCvzzaTXsq4KzayKEt5l57TTtoiujiDTNWPUkZHkrXwjvcqnnWYdJUtqMklTKiWW6t7PfWZfYormphkf1JGOmsRFV69%252BwTYT34tf399jO4JBHQ2BjvE9Wdou1n0T61Imy0XdwpwhdywgOyI49gKweMSbIqRIC4Z8bjYC621D%252FFifKNNLDPplaXijBQQgMboAOf8eODSCuWdnFClwomZCM%252FIT%252BLnpjhMWZyNSmLqr%252F0u7K21HpP2cEwYTWInQQ1M4KB5wKkBgWeDehAdnR9OGBxSCuom90Y%252FoMB8r3LNpEUaDhOh");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
