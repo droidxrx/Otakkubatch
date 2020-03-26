145b49fe4d58b597b4b18cf481a423828bef2fda
droidxrx <61324913+droidxrx@users.noreply.github.com>
2020-03-26 20:52:21 +0700

    Create Violet Evergarden Gaiden Eien to Jidou Shuki Ningyou BD Subtitle Indonesia.js

---
+++ video/Violet Evergarden Gaiden Eien to Jidou Shuki Ningyou BD Subtitle Indonesia.js
@@ -0,0 +1,21 @@
+$('.dropbtn').attr('onClick', 'MyRemove()');
+function MyRemove() {$("#nonton-bro").empty()}
+//1080p
+function eps11080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
+    ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=hbDmPXLQbHnbksRti3T8gQuGyC9qVaMUkEaHp1b1po7qtn1NN%252FtNnC5PQKB5WpGoPZmAl%252Bw%252FE4sR2%252F5B1u46vnWgAkhp1kXFBfYU%252FtzW1qnIC%252BQNlq2mdnIwxbnNzj%252BJM%252BKI0o%252FlIMLJqnNirJ5yCgJO5oZpfexycqWkeoeK198cHTL4DeKxud9HvOL9Yk4U6ryRejyTYQ9wOMmneUk4h8bbUxUFjUWTqUF3n3PuFSKmC6EXaU6X3g762AWlHy285a92yv9Lqou8HcmKpxVf1Lji08HpJq3bXH6w3ZAleqX15ksOlyomqTdCenqHl%252FFRFJa60hSJpVNAXWwNn9jLli");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps21080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
+    ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=n7BnjARvlq3MRmyrywrYIw2xnXnWw7ESGRZQqoK2JSitXV6MjoBX2q2QVuIZTeU%252FCUMdGOY75VeNhu4q9MPWXDfQF%252FbJRBn9QzFZRpeqsAupAgmAQXuEzy7mySnmMiF0NyYcCoL11Jiz7MpHx3JjHxaYm82qR7ItQ6OKUAEd27EBoYV9V2GinC571nV5GNi%252BH%252BNstQPfj5UNnj6KiZBGHA0ebx1eGQFf5Q%252BfzxIawyBXk0SpPDv6fnah%252B8BxD6AV9JyJui5wyRSoofOpii%252F02tXRXotQS3Pl2C2SoIANlzAIGWKNGF%252Bx66DB9kg3aicXb2IKeJ4cdqH%252FRcmHCjAoST");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+
+//720p
+function eps1720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
+    ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=28Xm3DgDkZ5UsKExfMSxjwzHW7fKuB%252FIIvux%252BhfNLA1s4SegIpbsPHUh90hIrPAXFZfjNm5RhT7uNe5QyXw7pRAmzp8V1a2QhMpX3z6K7dm8CVVtOVyHvRK%252BeWRCU8EH7kLaMFtATDv2b5ZxLS89WQFMBTitbA5Wuuiiato8nms5GnS%252Fvfp%252FcnmoYJPbIFwBBAoqSeNVHTpcV%252FT%252F327M1S9GTZrvtiHfg4HyfRrp0xHWAYuwIrvK42bV7srxEXRzZJfJzqUx8hebXtyCiAONJQ9OHo1eO8KgG%252FJ0z9Z4%252B5Kvtm%252BqugwuKXgn8mS6NDoCcEpg9a1zbJ6Zj9QZuV6m5T");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps2720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
+    ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=jJVDWxy6pOIWoey%252FKfgQNwlsSvi2%252F0lJJqz0dbVJxCm4CR2497%252BfXHS4zapIzZ5E6FWBJsM4H6M2ExBSbUky4G5%252BHX0EyV7NLGjdhUslMCw%252FMkVOOLtuY3G2%252B%252FslU80hQ8Wj%252F5vU%252BjC5ujHxkAWPokF91gAGx18sbRA9qGaY7WgG1G1wn6SIfdzUjsPhLG4MWs2lGBLttq1aVBrZNrh0nHVtXZY5i%252BHngQPYze6qXkYimqKvO99sZ9iTik0IIDBFlZwdRPy3rlRNFgfewU94xRBEIzcRA65AR2usYU7CimCxPk3nvHHkD%252FUu7X2C91b0nyWDjrORM%252BgaZmkOvaPVzY");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
