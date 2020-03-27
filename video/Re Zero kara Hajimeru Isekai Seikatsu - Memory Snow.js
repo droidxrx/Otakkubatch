9f34672fb51afb50023087ba285168100310a341
droidxrx <61324913+droidxrx@users.noreply.github.com>
2020-03-27 23:48:16 +0700

    Create Re Zero kara Hajimeru Isekai Seikatsu - Memory Snow.js

---
+++ video/Re Zero kara Hajimeru Isekai Seikatsu - Memory Snow.js
@@ -0,0 +1,21 @@
+$('.dropbtn').attr('onClick', 'MyRemove()');
+function MyRemove() {$("#nonton-bro").empty()}
+//1080p
+function eps11080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
+    ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=mFIQ%252B0XQ5iuirm1U84QmCwJGdC7RPyi%252F900G1peHw%252BJjxl8b6eETUcOwQPQrifNf44s1sa23GVtuWvhnS7%252Fsf0L2LeYXrS%252Fj0J62AKfVIndt3C3SDmsr3ZG2ZpOSiUoPWCCgr2WEJO0EPEIi9bR6KCoKVwuFjLmyxwpY3ixnIPW0pwPQ8xqLjB9HgPQEjV536wtS6Gl82OWCjsDd9%252Fp9Idjm%252FFaBA0FSxLRW9frbcFd731qLjckV7KnddEioTmPkhiW9isVuYe9RpjUnJxqTw8NUrY%252F4zukXXJPzbxsrSlbaxE5pNyWA7IOi%252FWuZSycmMYXM0MWBu892MYxhFJ9bdM");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps21080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
+    ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=yyBJDTTpwcD5xTLQlK4c%252BQFUVvLJUF%252Fn%252B388o%252FAbIaEbkJr9iyUg%252F7NBWwMtP0lpJPW0r7Vgh4BYz9pN9Aq7YQ892ZvkxFkPf%252B67Q2eoJkoWAM9na8sxEhS7vK0jDZRW2EoXDgORZESSf1zKe%252Fz4M1s77a51WpWQgdAdFRwhiaD86eVFlFL8rqFGr%252BIxlheFyNBqpnNeT7Cxpe1Gp%252BDcEWvxyM2jnheH8dNI6WeuiBSAU4yuHXQVjAo8JmCglCeehk8p9AUaxVclQjWk1exRobkAIYviSnE%252B9EIMzHvSskXcI3jERmKIvwLtYDyeBUWktDCvi1SvDXPNS2TOmYIofZ");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+
+//720p
+function eps1720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
+    ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=pRz5eGO%252BntoBBisGAkqJiwaZaoU5YSMXqAdcuXbEVLzSbvJVpMAFK1Kioq3BOJxIjI3poMTfnE0se1m8RPdmsfVQoNlqndkp5X4WaUcFSbvFBJW%252BD4zAagEHw4oehYaTA4SG%252Fg9vkDI1wYLWBbXRpVRZWzUvzA0fXovRBEXJHBzte4UeynPcy99eqp51yLNuPoP2rYj5uLAmuPA8XbjSx8a9qrMJ3mcRSLoBbj%252ByVIxADksOF0a2dUuR%252Bhtl8RwOXJEKYVfwslaBkHWLI4VCYnHtNK0HMKM3RqIeLNjuUIBPKKLe7CENua%252FrkJ%252F3VLA2JhcShkHeSwyJFkOdGcgaT%252B");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
+function eps2720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
+    ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=qPsSk100QvjDCrO5gz5OPA1lgNWEWvCV8VOR70IWf2VrVvL2ZsIjF9aDkazTbClxmnZOoBr3ups%252FTRChAOEOlb7k7q%252FwegfEc2cRBoaecq2p4T%252BxDtl%252FCEHO%252FTrEtYsCEdC8WtWLRzAdvfUW0mKpbesCs6FSbuzDijCLwJGh%252Bcrffj8Sn5FF%252BpVeVUMAYLLg1D6LtUtqd3m0RBGKmLQ%252BB14lCRhQ5owIYpbBoxplvwNSIaMe6ZOYZ0w2VBCl6Z5f%252BQ6FH9GdrecmSQJpv9oIB0ZYquC6mXikxRaKLtWCzGaAURhjM92hwWwcpvVHeNUi%252FQ7Mvor8ajYNQNWUaIhpP2");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
