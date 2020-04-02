a50d003d4d4e71c9844e4e441be2efd7240afe4a
droidxrx <61324913+droidxrx@users.noreply.github.com>
2020-04-02 22:53:28 +0700

    Update plunderer.js

--- video/plunderer.js
+++ video/plunderer.js
@@ -41,6 +41,10 @@ function eps101080P() {
     $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
     ifrm.setAttribute("src","https://muvix.us/video/5aWGAiRcFtl9pUE/");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
 }
+function eps111080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
+    ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=wjZcolVp3svD6bzf4sTUBQLK7Yqdjm8Sid8OXg%252FUrCh3%252F%252F6XYimq8lKwV9uMs%252F5%252BIdHiXA0vVgqVhvPTRg8HtN5WTnVUgHTwhfnN9zohpq4eqzF%252BIMbxCy%252BPdOOEX4Www76uhXUaz9WzUgckJD80ijjwQyfQm3UBHccB56pZARy0I15WFz%252FWG1gGvvHIz4tEri9U8iyqGGOatzd1%252Frkc6afasL8tVOuw0M5BaQeyzBVyZIn%252FH6kqfGM2qR8zMzDm0JC29H%252FO9SBwvLFLt2H%252BU6o%252B9gnaP%252FsfYvTzYZfoJ1TGkX%252B1tGbqob%252F1WfPAjOpTfCvc4gpmyCI091EPdRElca");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
 //720p
 function eps1720P() {
     $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
@@ -81,4 +85,8 @@ function eps9720P() {
 function eps10720P() {
     $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
     ifrm.setAttribute("src","https://muvix.us/video/KMyata7woRdtjui/");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps11720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
+    ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=CFBk8smHVutpOdr1Ioul2QcLCbbnsGVBGc4J0IshNNm6KhAfT8%252FuEuaVztP6r97JI9jCrJzGSRi%252FWIFomtDDFs3ervNZvBKQnCZ79HfoFD%252BNSFRnUuUT2AqJQwt27cdNsfKCCLXKqo4TM5ENe8GTHp6Dl3MxZAUafpYHeSjCpQoGrr6YY4T00018dxMijCWHdN%252FP9R0E6FQI6IFHv2d1bnB%252FVem3ZClXlFHzSDubwxlPmsz9ltiwV4ZjBt9WlzAC%252FOIqUseI8tnYF0HY2LjsW0bzpI802hHNRkAW%252B%252FkJt7Pj2YIUCzZ5EYLt7K%252BGzVByp4feLwoqKFg%252F%252F3qQL3CZky");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
 }
