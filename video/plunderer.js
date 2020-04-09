ac911499db0ceed8c217f0532cda1eb1ee16e836
droidxrx <61324913+droidxrx@users.noreply.github.com>
2020-04-09 10:23:35 +0700

    Update plunderer.js

--- video/plunderer.js
+++ video/plunderer.js
@@ -45,6 +45,10 @@ function eps111080P() {
     $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
     ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=wjZcolVp3svD6bzf4sTUBQLK7Yqdjm8Sid8OXg%252FUrCh3%252F%252F6XYimq8lKwV9uMs%252F5%252BIdHiXA0vVgqVhvPTRg8HtN5WTnVUgHTwhfnN9zohpq4eqzF%252BIMbxCy%252BPdOOEX4Www76uhXUaz9WzUgckJD80ijjwQyfQm3UBHccB56pZARy0I15WFz%252FWG1gGvvHIz4tEri9U8iyqGGOatzd1%252Frkc6afasL8tVOuw0M5BaQeyzBVyZIn%252FH6kqfGM2qR8zMzDm0JC29H%252FO9SBwvLFLt2H%252BU6o%252B9gnaP%252FsfYvTzYZfoJ1TGkX%252B1tGbqob%252F1WfPAjOpTfCvc4gpmyCI091EPdRElca");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
 }
+function eps121080P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
+    ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=zyvkUFh7Og6VmzY9ULFqfQjLtONKszxgTeMvxoXwB5%252FFNB2qRE2DPp3YYrBLPX%252BNWhxKmYQ03UXfOTjBJPhe%252B4mZoRANsMaNZLrHGkpmrVahik%252Bo204QKH6WsuUPUHdWd5oENZvYuSf2qPzjg6IWd%252Bjo2wkJDo9L9tBzFQ1TqkOv505d5%252F81ol25SEPxm5EAQs5h0qkZq1EjUDAfVPsnFQihaEnLvrPVlQikmEmW%252FhEbP3nkZ6bIrV1L7n8PByFeU2KFrtvLofhTbQa4ifyqrjiFHJ%252BHYpJ0%252FjCvk6mDOgqzHLriBn%252FlpEoLKRZqlXdD5INCUFE0ZYQZDpQsV%252Bk44h");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
+}
 //720p
 function eps1720P() {
     $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
@@ -89,4 +93,8 @@ function eps10720P() {
 function eps11720P() {
     $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
     ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=CFBk8smHVutpOdr1Ioul2QcLCbbnsGVBGc4J0IshNNm6KhAfT8%252FuEuaVztP6r97JI9jCrJzGSRi%252FWIFomtDDFs3ervNZvBKQnCZ79HfoFD%252BNSFRnUuUT2AqJQwt27cdNsfKCCLXKqo4TM5ENe8GTHp6Dl3MxZAUafpYHeSjCpQoGrr6YY4T00018dxMijCWHdN%252FP9R0E6FQI6IFHv2d1bnB%252FVem3ZClXlFHzSDubwxlPmsz9ltiwV4ZjBt9WlzAC%252FOIqUseI8tnYF0HY2LjsW0bzpI802hHNRkAW%252B%252FkJt7Pj2YIUCzZ5EYLt7K%252BGzVByp4feLwoqKFg%252F%252F3qQL3CZky");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm)
+}
+function eps12720P() {
+    $("#nonton-bro").empty();document.getElementById("nonton-bro").innerHTML = "<div class=\"lds-ripple2\"><div class=\"lds-ripple\"><div></div><div></div></div></div>";var ifrm=document.createElement("iframe");ifrm.setAttribute("class","nonton-bro-item");
+    ifrm.setAttribute("src","//gdriveplayer.me/embed2.php?link=p6S9BhOJ7LEJaZ6Fd1oB1wkDDFUKV5HaJXzQWNnVXMvtCI0R4IPvLEfw0EygP9LlNvKXsx8SWmP0Ym6HKB8KhxNJsT6oSNzyy3B8xynEXpb%252Bxe0guJwSfzDSlPsrX76LZPx%252B4ZbUxBuJMviNuGi0H8lsTiPIJeW29QURGd%252Fhuj1JilONNfF%252BPFGy36%252Ft4%252BsEoD1kg0or42b1w6I6ZRr8nSNtSE7ovPtAdIm6RLPQgicTTm3AKcRAOIcGuD9oLLphZ%252BMoSm1abE2Mp%252BwoBfnXzZSaMPkIDdffQTMNn%252BClIs8jo13D9%252FB2BKJJ0YKrj9ioZuKWk7NCJmcrTL6S6mjKWo");ifrm.setAttribute("scrolling","no");ifrm.setAttribute("allowfullscreen","true");document.getElementById("nonton-bro").appendChild(ifrm);
 }
